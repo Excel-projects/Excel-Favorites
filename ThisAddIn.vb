99958ce3cc29c6947a9caaebf20a9b530eff3a77
aduguid <anthonyduguid@gmail.com>
2017-06-08 10:42:39 +1000

    Initial commit

---
+++ ThisAddIn.vb
@@ -0,0 +1,17 @@
+﻿Imports Favorites.Code
+ 
+Public Class ThisAddIn
+
+    Protected Overrides Function CreateRibbonExtensibilityObject() As Microsoft.Office.Core.IRibbonExtensibility
+        Return New Ribbon()
+    End Function
+
+    Private Sub ThisAddIn_Startup() Handles Me.Startup
+
+    End Sub
+
+    Private Sub ThisAddIn_Shutdown() Handles Me.Shutdown
+
+    End Sub
+
+End Class
