b540587667cb80311eb414b0f235cc2b35ff91f1
tduguid <tduguid@NB61300907>
2017-12-19 09:36:19 +1100

    Updated formatting

--- Scripts/ErrorHandler.vb
+++ Scripts/ErrorHandler.vb
@@ -6,19 +6,19 @@ Imports Excel = Microsoft.Office.Interop.Excel
 
 Namespace Scripts
 
-        Public Class ErrorHandler
+    Public Class ErrorHandler
 
-            Public Shared Sub DisplayMessage(ex As Exception)
-                Dim sf As New System.Diagnostics.StackFrame(1)
-                Dim caller As System.Reflection.MethodBase = sf.GetMethod()
-                Dim currentProcedure As String = (caller.Name).Trim()
-                Dim errorMessageDescription As String = ex.ToString()
-                errorMessageDescription = System.Text.RegularExpressions.Regex.Replace(errorMessageDescription, "\r\n+", " ")
-                Dim msg As String = "Contact your system administrator. A record has been created in the log file." + Environment.NewLine
-                msg += (Convert.ToString("Procedure: ") & currentProcedure) + Environment.NewLine
-                msg += "Description: " + ex.ToString() + Environment.NewLine
-                MessageBox.Show(msg, "Unexpected Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
-            End Sub
+        Public Shared Sub DisplayMessage(ex As Exception)
+            Dim sf As New System.Diagnostics.StackFrame(1)
+            Dim caller As System.Reflection.MethodBase = sf.GetMethod()
+            Dim currentProcedure As String = (caller.Name).Trim()
+            Dim errorMessageDescription As String = ex.ToString()
+            errorMessageDescription = System.Text.RegularExpressions.Regex.Replace(errorMessageDescription, "\r\n+", " ")
+            Dim msg As String = "Contact your system administrator. A record has been created in the log file." + Environment.NewLine
+            msg += (Convert.ToString("Procedure: ") & currentProcedure) + Environment.NewLine
+            msg += "Description: " + ex.ToString() + Environment.NewLine
+            MessageBox.Show(msg, "Unexpected Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Sub
 
         Public Shared Function IsActiveDocument(Optional showMsg As Boolean = False) As Boolean
             Try
@@ -105,4 +105,4 @@ Namespace Scripts
 
     End Class
 
-    End Namespace
+End Namespace
