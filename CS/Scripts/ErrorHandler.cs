5042ebc6e008454d7fdcd4de2ed5e23ef2eac9e8
aduguid <anthonyduguid@gmail.com>
2020-08-28 14:15:08 +1000

    Removed comments after watching a great lecture by Kevlin Henney - Se…
    …ven Ineffective Coding Habits of Many Programmers
    
    https://www.youtube.com/watch?v=ZsHMHukIlJY

--- CS/Scripts/ErrorHandler.cs
+++ CS/Scripts/ErrorHandler.cs
@@ -7,21 +7,12 @@ using log4net.Config;
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
-// <summary> 
-// This namespaces if for generic application classes
-// </summary>
 namespace Favorites.Scripts
 {
-    /// <summary> 
-    /// Used to handle exceptions
-    /// </summary>
     public class ErrorHandler
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(ErrorHandler));
 
-        /// <summary>
-        /// Applies a new path for the log file by FileAppender name
-        /// </summary>
         public static void SetLogPath()
         {
             XmlConfigurator.Configure();
@@ -41,9 +32,6 @@ namespace Favorites.Scripts
             }
         }
 
-        /// <summary>
-        /// Create a log record to track which methods are being used.
-        /// </summary>
         public static void CreateLogRecord()
         {
             try
@@ -60,17 +48,6 @@ namespace Favorites.Scripts
             }
         }
 
-        /// <summary> 
-        /// Used to produce an error message and create a log record
-        /// <example>
-        /// <code lang="C#">
-        /// ErrorHandler.DisplayMessage(ex);
-        /// </code>
-        /// </example> 
-        /// </summary>
-        /// <param name="ex">Represents errors that occur during application execution.</param>
-        /// <param name="isSilent">Used to show a message to the user and log an error record or just log a record.</param>
-        /// <remarks></remarks>
         public static void DisplayMessage(Exception ex, Boolean isSilent = false)
         {
             System.Diagnostics.StackFrame sf = new System.Diagnostics.StackFrame(1);
@@ -89,12 +66,6 @@ namespace Favorites.Scripts
             }
         }
 
-        /// <summary> 
-        /// Check to see if there is an active document
-        /// </summary>
-        /// <param name="showMsg">To show a message </param>
-        /// <returns>A method that returns true or false if there is an active document </returns> 
-        /// <remarks></remarks>
         public static bool IsActiveDocument(bool showMsg = false)
         {
             try
@@ -119,12 +90,6 @@ namespace Favorites.Scripts
             }
         }
 
-        /// <summary> 
-        /// Check to see if there is an active selection
-        /// </summary>
-        /// <param name="showMsg">To show a message </param>
-        /// <returns>A method that returns true or false if there is an active selection </returns> 
-        /// <remarks></remarks>
         public static bool IsActiveSelection(bool showMsg = false)
         {
             Excel.Range checkRange = null;
@@ -156,12 +121,6 @@ namespace Favorites.Scripts
             }
         }
 
-        /// <summary> 
-        /// Check to see if there is an named range selected
-        /// </summary>
-        /// <param name="showMsg">To show a message </param>
-        /// <returns>A method that returns true or false if there is a valid list object </returns> 
-        /// <remarks></remarks>
         public static bool IsValidListObject(bool showMsg = false)
         {
             Excel.ListObject tbl = null;
@@ -192,13 +151,6 @@ namespace Favorites.Scripts
             }
         }
 
-        /// <summary>
-        /// This method check whether Excel is in Cell Editing mode or not
-        /// There are few ways to check this (eg. check to see if a standard menu item is disabled etc.)
-        /// I know in cell editing mode app.DisplayAlerts throws an Exception, so here I'm relying on that behaviour
-        /// </summary>
-        /// <param name="showMsg">To show a message </param>
-        /// <returns>true if Excel is in cell editing mode</returns>
         private static bool IsInCellEditingMode(bool showMsg = false)
         {
             bool flag = false;
@@ -217,12 +169,6 @@ namespace Favorites.Scripts
             return flag;
         }
 
-        /// <summary> 
-        /// Can an object be inserted
-        /// </summary>
-        /// <param name="showMsg">To show a message </param>
-        /// <returns>A method that returns true or false if an object can be enabled </returns> 
-        /// <remarks></remarks>
         public static bool IsEnabled(bool showMsg = false)
         {
             try
@@ -257,12 +203,6 @@ namespace Favorites.Scripts
             }
         }
 
-        /// <summary> 
-        /// Can an object be inserted
-        /// </summary>
-        /// <param name="showMsg">To show a message </param>
-        /// <returns>A method that returns true or false if an object can be used </returns> 
-        /// <remarks></remarks>
         public static bool IsAvailable(bool showMsg = false)
         {
             try
@@ -290,12 +230,6 @@ namespace Favorites.Scripts
             }
         }
 
-        /// <summary> 
-        /// Check if the object is a date     
-        /// </summary>
-        /// <param name="expression">Represents the cell value </param>
-        /// <returns>A method that returns true or false if there is a valid date </returns> 
-        /// <remarks></remarks>
         public static bool IsDate(object expression)
         {
             if (expression != null)
